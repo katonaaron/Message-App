0a3a3ee18d318d163773c3927cdd5d254c1f28fd
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2019-03-01 23:32:05 +0200

    UtlityLib: error
    Function for error printing

---
+++ UtilityLib/error.c
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "error.h"
+
+void PrintError(CM_ERROR Code, const TCHAR * Function)
+{
+    _tprintf_s(TEXT("Unexpected error: %s failed with error code: 0x%X: \n"), Function, Code);
+}
---
+++ UtilityLib/error.h
@@ -0,0 +1,9 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#include "stdafx.h"
+#include <tchar.h>
+
+void PrintError(CM_ERROR Code, const TCHAR* Function);
+
+#endif // _ERROR_H_
