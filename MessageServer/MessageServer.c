f9254a5ce8c178a323fe5f7c8460751bc57244ea
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2019-03-07 00:01:59 +0200

    MessageServer -implementation
    -needs to be expanded and corrected

--- MessageServer/MessageServer.c
+++ MessageServer/MessageServer.c
@@ -5,6 +5,10 @@
 #include "UtilityLib.h"
 #include "handle_clients.h"
 
+#define MAX_THREADS 10
+#define MIN_THREADS 1
+
+INT64 gCurrentConnections = 0;
 
 int ReadMaxConnections(TCHAR* String, INT64* MaxConnections)
 {
@@ -33,14 +37,22 @@ int ReadMaxConnections(TCHAR* String, INT64* MaxConnections)
 
 int _tmain(int argc, TCHAR* argv[])
 {
-    INT retVal = 0;
-    UINT rollback = 0;
+    INT retVal = 0, rollback = 0;
     CM_ERROR error = CM_SUCCESS;
-    DWORD result = 0;
-    CM_SERVER_DATA server_data = {NULL, 0, NULL};
-    HANDLE commandThread = NULL;
 
-    if (argc < 2 || ReadMaxConnections(argv[1], &server_data.MaxConnections) != 0)
+    CM_SERVER* server = NULL;
+    CM_SERVER_CLIENT* client = NULL;
+    CM_SERVER_CLIENT_DATA data;
+    CM_DATA_BUFFER* received = NULL;
+    INT64 maxConnections;
+
+    //Threadpool variables
+    PTP_POOL pool = NULL;
+    PTP_WORK work = NULL;
+    TP_CALLBACK_ENVIRON callBackEnviron;
+    PTP_CLEANUP_GROUP cleanupgroup = NULL;
+
+    if (argc < 2 || ReadMaxConnections(argv[1], &maxConnections) != 0)
     {
         _tprintf_s(TEXT("Error: invalid maximum number of connections\n"));
         retVal = -1;
@@ -60,7 +72,7 @@ int _tmain(int argc, TCHAR* argv[])
     }
     rollback++;
     
-    error = CreateServer(&server_data.Server);
+    error = CreateServer(&server);
     if (CM_IS_ERROR(error))
     {
         PrintError(error, TEXT("CreateServer"));
@@ -69,67 +81,89 @@ int _tmain(int argc, TCHAR* argv[])
     }
     rollback++;
 
-    server_data.EndEvent = CreateEvent(
-        NULL,               // default security attributes
-        TRUE,               // manual-reset event
-        FALSE,              // initial state is nonsignaled
-        NULL                // create without a name
-    );
-    if (NULL == server_data.EndEvent)
+    error = CreateDataBuffer(&received, sizeof(CM_MESSAGE) + MAX_BUFFER_SIZE);
+    if (CM_IS_ERROR(error))
     {
-        PrintError(GetLastError(), TEXT("CreateEvent"));
+        PrintError(error, TEXT("CreateDataBuffer"));
         retVal = -1;
         goto main_cleanup;
     }
     rollback++;
 
-    commandThread = CreateThread(
-        NULL,                   // default security attributes
-        0,                      // use default stack size  
-        ReceiveClients,         // thread function name
-        &server_data,           // argument to thread function 
-        0,                      // use default creation flags 
-        NULL                    // thread identifier not needed
-    );
-    if (NULL == commandThread)
-    {
-        PrintError(GetLastError(), TEXT("CreateThread"));
+    InitializeThreadpoolEnvironment(&callBackEnviron);
+    pool = CreateThreadpool(NULL);
+    if (NULL == pool) {
+        PrintError(GetLastError(), TEXT("CreateThreadpool"));
         retVal = -1;
         goto main_cleanup;
     }
     rollback++;
 
-    TCHAR in = ' ';
-    while (in != L'Q' && in != L'q')
+    SetThreadpoolThreadMaximum(pool, MIN_THREADS);
+    if (!SetThreadpoolThreadMinimum(pool, MAX_THREADS))
     {
-        _tscanf_s(TEXT("%c"), &in, 1);
-    }
-
-    if (!SetEvent(server_data.EndEvent))
-    {
-        PrintError(GetLastError(), TEXT("SetEvent"));
+        PrintError(GetLastError(), TEXT("SetThreadpoolThreadMinimum"));
         retVal = -1;
         goto main_cleanup;
     }
 
-    result = WaitForSingleObject(commandThread, INFINITE);
-    if (WAIT_FAILED == result)
-    {
-        PrintError(GetLastError(), TEXT("WaitForSingleObject"));
+    cleanupgroup = CreateThreadpoolCleanupGroup();
+    if (NULL == cleanupgroup) {
+        PrintError(GetLastError(), TEXT("CreateThreadpoolCleanupGroup"));
         retVal = -1;
         goto main_cleanup;
     }
+    rollback++;
 
+    SetThreadpoolCallbackPool(&callBackEnviron, pool);
+    SetThreadpoolCallbackCleanupGroup(&callBackEnviron, cleanupgroup, NULL);
+
+    data.Server = server;
+    while (TRUE)
+    {
+        error = AwaitNewClient(server, &client);
+        if (CM_IS_ERROR(error))
+        {
+            PrintError(error, TEXT("AwaitNewClient"));
+            retVal = -1;
+            goto main_cleanup;
+        }
+        rollback = 6;
+        data.Client = client;      
+
+        //TODO: set or list
+        data.Accept = gCurrentConnections < maxConnections;
+        if (data.Accept)
+        {
+            InterlockedIncrement64(&gCurrentConnections);
+        }
+
+        work = CreateThreadpoolWork(ProcessClient, &data, &callBackEnviron);
+        if (NULL == work) {
+            PrintError(error, TEXT("CreateThreadpoolWork"));
+            retVal = -1;
+            goto main_cleanup;
+        }
+        rollback = 7;
+
+        SubmitThreadpoolWork(work);
+    }
 
 main_cleanup:
     switch (rollback)
     {
+    case 7: //TODO 6,7 not clear
+        CloseThreadpoolCleanupGroupMembers(cleanupgroup, FALSE, NULL);
+    case 6:
+        AbandonClient(client);
+    case 5:
+        CloseThreadpoolCleanupGroup(cleanupgroup);
     case 4:
-        CloseHandle(commandThread);
+        CloseThreadpool(pool);
     case 3:
-        CloseHandle(server_data.EndEvent);
+        DestroyDataBuffer(received);
     case 2:
-        DestroyServer(server_data.Server);
+        DestroyServer(server);
     case 1:
         UninitCommunicationModule();
     default:
