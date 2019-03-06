ac2d09e62855cd0bdf88ac3cc989af21d27799ed
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2019-03-07 00:01:19 +0200

    handle_clients created
    This module represents the workers of the threadpool. They recieve data from the client and process it.

--- MessageServer/handle_clients.c
+++ MessageServer/handle_clients.c
@@ -1,23 +1,91 @@
 #include "stdafx.h"
 #include "handle_clients.h"
 
-DWORD WINAPI ReceiveClients(LPVOID Param)
+/*DWORD WINAPI ReceiveClients(LPVOID Param)
 {
     DWORD result = 1;
+    CM_ERROR error = CM_SUCCESS;
 
     CM_SERVER_DATA* server_data = (CM_SERVER_DATA*)Param;
-    UINT64 i = 0;
+    CM_SERVER_CLIENT* newClient = NULL;
 
-    while (TRUE)
+
+
+    do
     {
-        result = WaitForSingleObject(server_data->EndEvent, 0);
-        if (WAIT_OBJECT_0 == result)
+        error = AwaitNewClient(server_data->Server, &newClient);
+        if (CM_IS_ERROR(error))
         {
-            return 0;
+            PrintError(error, TEXT("AwaitNewClient"));
+            return 1;
         }
 
-        _tprintf_s(TEXT("%lld\n"), ++i);
-        Sleep(1000);
-    }
+        result = WaitForSingleObject(server_data->EndEvent, 0);
+        if (WAIT_FAILED == result)
+        {
+            PrintError(GetLastError(), TEXT("WaitForSingleObject"));
+            AbandonClient(newClient);
+            return 1;
+        }
+
+        //_tprintf_s(TEXT("%lld, 0x%X\n"), ++i, result);
+        //Sleep(1000);
+    } while (WAIT_OBJECT_0 != result);
     return 0;
+}*/
+
+VOID CALLBACK ProcessClient(PTP_CALLBACK_INSTANCE Instance, PVOID Parameter, PTP_WORK Work)
+{
+    UNREFERENCED_PARAMETER(Instance);
+    UNREFERENCED_PARAMETER(Work);
+
+    CM_SERVER_CLIENT_DATA* data = (CM_SERVER_CLIENT_DATA*)Parameter;
+    INT rollback = 0;
+
+    CM_MESSAGE* message = malloc(sizeof(CM_MESSAGE) + sizeof(BOOL));
+    message->Operation = CM_CONNECT;
+    message->Size = sizeof(BOOL);
+    memcpy(message->Buffer, &data->Accept, message->Size);
+    rollback = 1;
+
+    CM_DATA_BUFFER* dataToSend = NULL;
+    CM_ERROR error = CreateDataBuffer(&dataToSend, sizeof(CM_MESSAGE) + message->Size);
+    if (CM_IS_ERROR(error))
+    {
+        PrintError(error, TEXT("CreateDataBuffer"));
+        goto process_client_cleanup;
+    }
+    rollback = 2;
+
+    error = CopyDataIntoBuffer(dataToSend, (const CM_BYTE*)message, sizeof(CM_MESSAGE) + message->Size);
+    if (CM_IS_ERROR(error))
+    {
+        PrintError(error, TEXT("CopyDataIntoBuffer"));
+        goto process_client_cleanup;
+    }
+
+    CM_SIZE sendByteCount = 0;
+    error = SendDataToClient(data->Client, dataToSend, &sendByteCount);
+    if (CM_IS_ERROR(error))
+    {
+        PrintError(error, TEXT("SendDataToClient"));
+        goto process_client_cleanup;
+    }
+
+    _tprintf_s(TEXT("Successfully sent data from server client:\n \tSent data size: %d\n")
+        , sendByteCount
+    );
+
+process_client_cleanup:
+    switch (rollback)
+    {
+    case 2:
+        DestroyDataBuffer(dataToSend);
+    case 1:
+        free(message);
+    default:
+        //AbandonClient(data->Client);
+        break;
+    }
+    return;
 }
--- MessageServer/handle_clients.h
+++ MessageServer/handle_clients.h
@@ -3,11 +3,12 @@
 #include "stdafx.h"
 #include "UtilityLib.h"
 
-typedef struct _CM_SERVER_DATA
+typedef struct _CM_SERVER_CLIENT_DATA
 {
     CM_SERVER* Server;
-    INT64 MaxConnections;
-    HANDLE EndEvent;
-} CM_SERVER_DATA;
+    CM_SERVER_CLIENT* Client;
+    BOOL Accept;
+} CM_SERVER_CLIENT_DATA;
 
-DWORD WINAPI ReceiveClients(LPVOID Param);
+//DWORD WINAPI ReceiveClients(LPVOID Param);
+VOID CALLBACK ProcessClient(PTP_CALLBACK_INSTANCE Instance, PVOID Parameter, PTP_WORK  Work);
