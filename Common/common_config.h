9a17d60af5073988ea614a018398a2f128f616a6
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2019-03-16 00:30:34 +0200

    common macros refined

--- Common/common_config.h
+++ Common/common_config.h
@@ -2,9 +2,10 @@
 
 #include "common.h"
 
-#define GET_MESSAGE_SIZE(MessageBufferSize) (MessageBufferSize + sizeof(CM_MESSAGE))
+#define GET_MESSAGE_SIZE(MessageBufferSize) ((MessageBufferSize) + sizeof(CM_MESSAGE))
+#define GET_STRING_SIZE(StringLength) ((StringLength) * sizeof(TCHAR))
 
 #define MAX_INPUT_LENGTH 256
-#define MAX_INPUT_BUFFER_SIZE (MAX_INPUT_LENGTH * sizeof(TCHAR))
+#define MAX_INPUT_BUFFER_SIZE GET_STRING_SIZE(MAX_INPUT_LENGTH)
 #define MAX_MESSAGE_BUFFER_SIZE MAX_INPUT_BUFFER_SIZE
 #define MAX_MESSAGE_SIZE GET_MESSAGE_SIZE(MAX_MESSAGE_BUFFER_SIZE)
--- Common/common_error.h
+++ Common/common_error.h
@@ -2,5 +2,8 @@
 
 #include "common.h"
 
+#define CM_NOT_FOUND  MAKE_ERROR_CODE(CM_ERROR_SEVERITY, 0xA1)
+#define CM_MODULE_NOT_INITIALIZED MAKE_ERROR_CODE(CM_ERROR_SEVERITY, 0xA2)
+
 void PrintError(CM_ERROR Code, const TCHAR* Function);
 void PrintErrorMessage(const TCHAR* Message);
